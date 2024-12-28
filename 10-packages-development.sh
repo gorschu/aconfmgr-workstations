84708b04b8e376ab08c729a2e1dc4d53a0c0846b
Gordon Schulz <gordon@gordonschulz.de>
2024-12-28 12:56:05 +0100

    feat: Add development packages

---
+++ 10-packages-development.sh
@@ -0,0 +1,6 @@
+# shellcheck shell=bash
+AddPackage lazydocker # A simple terminal UI for docker and docker-compose, written in Go with the gocui library.
+AddPackage lazygit    # Simple terminal UI for git commands
+AddPackage nodejs     # Evented I/O for V8 javascript
+AddPackage npm        # JavaScript package manager
+AddPackage go         # Core compiler tools for the Go programming language

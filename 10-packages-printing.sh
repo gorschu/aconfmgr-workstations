6bf2b43be6601e6c2f3eda8d76e4d309bc87350b
Gordon Schulz <gordon@gordonschulz.de>
2024-12-28 12:37:52 +0100

    feat: Add CUPS

---
+++ 10-packages-printing.sh
@@ -0,0 +1,12 @@
+AddPackage cups                        # OpenPrinting CUPS - daemon package
+AddPackage cups-pdf                    # PDF printer for cups
+AddPackage epson-inkjet-printer-escpr2 # Epson Inkjet Printer Driver 2 (ESC/P-R) for Linux
+AddPackage --foreign vuescan-bin       # A powerful proprietary scanning tool developed by Hamrick Software
+
+CopyFile /etc/cups/ppd/EPSON_WF-4830_Series.ppd 640 '' cups
+CopyFile /etc/cups/ppd/EPSON_WF-4830_Series.ppd.O 640 '' cups
+CopyFile /etc/cups/printers.conf 600
+CreateFile /etc/cups/printers.conf.O 600 '' cups >/dev/null
+
+CreateLink /etc/systemd/system/sockets.target.wants/cups.socket /usr/lib/systemd/system/cups.socket
+SetFileProperty /etc/cups/classes.conf mode 600

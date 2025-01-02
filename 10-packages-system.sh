5caeceb06325030adb7951212f709591d72309ec
Gordon Schulz <gordon@gordonschulz.de>
2025-01-02 10:05:36 +0100

    chore: Move terminal packages

--- 10-packages-system.sh
+++ 10-packages-system.sh
@@ -1,14 +1,9 @@
 # shellcheck shell=bash
 AddPackage base                # Minimal package set to define a basic Arch Linux installation
 AddPackage base-devel          # Basic tools to build Arch Linux packages
-AddPackage bat                 # Cat clone with syntax highlighting and git integration
 AddPackage btrfs-progs         # Btrfs filesystem utilities
-AddPackage chezmoi             # Manage your dotfiles across multiple machines
 AddPackage efibootmgr          # Linux user-space application to modify the EFI Boot Manager
 AddPackage gptfdisk            # A text-mode partitioning tool that works on GUID Partition Table (GPT) disks
-AddPackage htop                # Interactive process viewer
-AddPackage just                # A handy way to save and run project-specific commands
-AddPackage less                # A terminal based program for viewing text files
 AddPackage linux               # The Linux kernel and modules
 AddPackage linux-firmware      # Firmware files for Linux
 AddPackage smartmontools       # Control and monitor S.M.A.R.T. enabled ATA and SCSI Hard Drives
@@ -20,11 +15,4 @@ AddPackage podman-compose # A script to run docker-compose.yml using podman
 
 AddPackage sbsigntools   # Tools to add signatures to EFI binaries and Drivers
 AddPackage systemd-ukify # Combine kernel and initrd into a signed Unified Kernel Image
-# neovim
-AddPackage neovim       # Fork of Vim aiming to improve user experience, plugins, and GUIs
-
-
 AddPackage linux-cachyos # The Linux BORE + LTO + AutoFDO Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel and modules
-
-AddPackage man-db # A utility for reading man pages
-AddPackage tldr   # Command line client for tldr, a collection of simplified man pages.
---
+++ 10-packages-terminal.sh
@@ -0,0 +1,18 @@
+# shellcheck shell=bash
+AddPackage bat             # Cat clone with syntax highlighting and git integration
+AddPackage chezmoi         # Manage your dotfiles across multiple machines
+AddPackage neovim          # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage tldr            # Command line client for tldr, a collection of simplified man pages.
+AddPackage zsh             # A very advanced and programmable command interpreter (shell) for UNIX
+AddPackage atuin           # Magical shell history
+AddPackage fd              # Simple, fast and user-friendly alternative to find
+AddPackage fzf             # Command-line fuzzy finder
+AddPackage starship        # The cross-shell prompt for astronauts
+AddPackage vivid           # LS_COLORS manager with multiple themes
+AddPackage zellij          # A terminal multiplexer
+AddPackage btop            # A monitor of system resources, bpytop ported to C++
+AddPackage htop            # Interactive process viewer
+AddPackage yubikey-manager # Python library and command line tool for configuring a YubiKey
+AddPackage topgrade        # Upgrade all the things
+AddPackage just            # A handy way to save and run project-specific commands
+AddPackage less            # A terminal based program for viewing text files

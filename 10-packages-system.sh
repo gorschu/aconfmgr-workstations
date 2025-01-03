5e514d015da2ca9fd4bf7751cfa2044f645d011c
Gordon Schulz <gordon@gordonschulz.de>
2025-01-03 13:43:48 +0100

    feat: Add incus and libvirt

--- 10-packages-system.sh
+++ 10-packages-system.sh
@@ -1,15 +1,16 @@
 # shellcheck shell=bash
-AddPackage base           # Minimal package set to define a basic Arch Linux installation
-AddPackage base-devel     # Basic tools to build Arch Linux packages
-AddPackage btrfs-progs    # Btrfs filesystem utilities
-AddPackage efibootmgr     # Linux user-space application to modify the EFI Boot Manager
-AddPackage fwupd          # Simple daemon to allow session software to update firmware
-AddPackage gptfdisk       # A text-mode partitioning tool that works on GUID Partition Table (GPT) disks
-AddPackage linux          # The Linux kernel and modules
-AddPackage linux-cachyos  # The Linux BORE + LTO + AutoFDO Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel and modules
-AddPackage linux-firmware # Firmware files for Linux
-AddPackage man-db         # A utility for reading man pages
-AddPackage sbsigntools    # Tools to add signatures to EFI binaries and Drivers
-AddPackage smartmontools  # Control and monitor S.M.A.R.T. enabled ATA and SCSI Hard Drives
-AddPackage systemd-ukify  # Combine kernel and initrd into a signed Unified Kernel Image
-AddPackage zram-generator # Systemd unit generator for zram devices
+AddPackage base                    # Minimal package set to define a basic Arch Linux installation
+AddPackage base-devel              # Basic tools to build Arch Linux packages
+AddPackage btrfs-progs             # Btrfs filesystem utilities
+AddPackage efibootmgr              # Linux user-space application to modify the EFI Boot Manager
+AddPackage fwupd                   # Simple daemon to allow session software to update firmware
+AddPackage gptfdisk                # A text-mode partitioning tool that works on GUID Partition Table (GPT) disks
+AddPackage linux                   # The Linux kernel and modules
+AddPackage linux-cachyos           # The Linux BORE + LTO + AutoFDO Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel and modules
+AddPackage linux-firmware          # Firmware files for Linux
+AddPackage man-db                  # A utility for reading man pages
+AddPackage sbsigntools             # Tools to add signatures to EFI binaries and Drivers
+AddPackage smartmontools           # Control and monitor S.M.A.R.T. enabled ATA and SCSI Hard Drives
+AddPackage systemd-ukify           # Combine kernel and initrd into a signed Unified Kernel Image
+AddPackage thin-provisioning-tools # Suite of tools for manipulating the metadata of the dm-thin device-mapper target
+AddPackage zram-generator          # Systemd unit generator for zram devices
--- 10-packages-virtualization-containers.sh
+++ 10-packages-virtualization-containers.sh
@@ -1,5 +1,19 @@
 # shellcheck shell=bash
 AddPackage crun           # A fast and lightweight fully featured OCI runtime and C library for running containers
+AddPackage incus          # Powerful system container and virtual machine manager
+AddPackage libvirt        # API for controlling virtualization engines (openvz,kvm,qemu,virtualbox,xen,etc)
 AddPackage podman         # Tool and library for running OCI-based containers in pods
 AddPackage podman-compose # A script to run docker-compose.yml using podman
+AddPackage qemu-full      # A full QEMU setup
+AddPackage skopeo         # A command line utility for various operations on container images and image repositories.
+AddPackage swtpm          # Libtpms-based TPM emulator with socket, character device, and Linux CUSE interface
+AddPackage umoci          # Umoci Modifies Open Container Images
 AddPackage virt-what      # A small program that prints out a list of facts about a virtual machine
+
+CreateLink /etc/systemd/system/sockets.target.wants/incus-user.socket /usr/lib/systemd/system/incus-user.socket
+CreateLink /etc/systemd/system/sockets.target.wants/incus.socket /usr/lib/systemd/system/incus.socket
+CreateLink /etc/systemd/system/sockets.target.wants/libvirtd-admin.socket /usr/lib/systemd/system/libvirtd-admin.socket
+CreateLink /etc/systemd/system/sockets.target.wants/libvirtd-ro.socket /usr/lib/systemd/system/libvirtd-ro.socket
+CreateLink /etc/systemd/system/sockets.target.wants/libvirtd.socket /usr/lib/systemd/system/libvirtd.socket
+
+SetFileProperty /var/lib/swtpm-localca owner tss

037e33678e4a181d6da3815ba9468121abe15a67
Gordon Schulz <gordon@gordonschulz.de>
2025-01-11 07:52:32 +0100

    feat: add dosfstools

--- 10-packages-system.sh
+++ 10-packages-system.sh
@@ -2,6 +2,7 @@
 AddPackage base                    # Minimal package set to define a basic Arch Linux installation
 AddPackage base-devel              # Basic tools to build Arch Linux packages
 AddPackage btrfs-progs             # Btrfs filesystem utilities
+AddPackage dosfstools              # DOS filesystem utilities
 AddPackage efibootmgr              # Linux user-space application to modify the EFI Boot Manager
 AddPackage fwupd                   # Simple daemon to allow session software to update firmware
 AddPackage gptfdisk                # A text-mode partitioning tool that works on GUID Partition Table (GPT) disks

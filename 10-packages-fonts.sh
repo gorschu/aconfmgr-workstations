cce9eaa775fd225d0aafff0d10ebb0b72c4187d3
Gordon Schulz <gordon@gordonschulz.de>
2025-01-16 11:49:01 +0100

    feat: add ttf-liberation

--- 10-packages-fonts.sh
+++ 10-packages-fonts.sh
@@ -3,6 +3,7 @@ AddPackage noto-fonts
 AddPackage noto-fonts-emoji
 AddPackage ttf-dejavu              # Font family based on the Bitstream Vera Fonts with a wider range of characters
 AddPackage ttf-jetbrains-mono-nerd # Patched font JetBrains Mono from nerd fonts library
+AddPackage ttf-liberation          # Font family which aims at metric compatibility with Arial, Times New Roman, and Courier New
 
 CreateLink /etc/fonts/conf.d/10-hinting-slight.conf /usr/share/fontconfig/conf.default/10-hinting-slight.conf
 CreateLink /etc/fonts/conf.d/10-scale-bitmap-fonts.conf /usr/share/fontconfig/conf.default/10-scale-bitmap-fonts.conf

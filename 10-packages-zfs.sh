e563e29a3dd8de326a5fea62fe798dbffa4aa6cb
Gordon Schulz <gordon@gordonschulz.de>
2025-01-16 07:53:16 +0100

    feat: Add ZFS specific stuff

--- 10-packages-zfs.sh
+++ 10-packages-zfs.sh
@@ -1,2 +1,3 @@
 AddPackage linux-cachyos-zfs # zfs module for the Linux BORE + LTO + AutoFDO Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel
 AddPackage zfs-utils         # Userspace utilities for the Zettabyte File System.
+AddPackage zrepl-git         # One-stop ZFS backup & replication solution

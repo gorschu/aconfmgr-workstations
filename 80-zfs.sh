418addae6596392fb00940cc1ad0a0c961958f0a
Gordon Schulz <gordon@gordonschulz.de>
2025-01-16 12:05:52 +0100

    feat: only apply ZFS configuration when ZFS module is actually in play

--- 80-zfs.sh
+++ 80-zfs.sh
@@ -1,14 +1,17 @@
 # shellcheck shell=bash
-CopyFile /etc/modprobe.d/zfs.conf
-CopyFile "/etc/zfs/zfskey_rpool_$(hostnamectl hostname)" 600
-CopyFile /etc/systemd/system/zfs-load-key.service
+if grep -q zfs /proc/modules; then
+  CopyFile /etc/modprobe.d/zfs.conf
+  CopyFile "/etc/zfs/zfskey_rpool_$(hostnamectl hostname)" 600
+  CopyFile /etc/systemd/system/zfs-load-key.service
+  IgnorePath /etc/zfs/zpool.cache
 
-CreateLink /etc/systemd/system/multi-user.target.wants/zfs.target /usr/lib/systemd/system/zfs.target
-CreateLink /etc/systemd/system/zfs-import.target.wants/zfs-import-scan.service /usr/lib/systemd/system/zfs-import-scan.service
-CreateLink /etc/systemd/system/zfs-mount.service.wants/zfs-load-key.service /etc/systemd/system/zfs-load-key.service
-CreateLink /etc/systemd/system/zfs-volumes.target.wants/zfs-volume-wait.service /usr/lib/systemd/system/zfs-volume-wait.service
-CreateLink /etc/systemd/system/zfs.target.wants/zfs-import.target /usr/lib/systemd/system/zfs-import.target
-CreateLink /etc/systemd/system/zfs.target.wants/zfs-mount.service /usr/lib/systemd/system/zfs-mount.service
-CreateLink /etc/systemd/system/zfs.target.wants/zfs-volumes.target /usr/lib/systemd/system/zfs-volumes.target
+  CreateLink /etc/systemd/system/multi-user.target.wants/zfs.target /usr/lib/systemd/system/zfs.target
+  CreateLink /etc/systemd/system/zfs-import.target.wants/zfs-import-scan.service /usr/lib/systemd/system/zfs-import-scan.service
+  CreateLink /etc/systemd/system/zfs-mount.service.wants/zfs-load-key.service /etc/systemd/system/zfs-load-key.service
+  CreateLink /etc/systemd/system/zfs-volumes.target.wants/zfs-volume-wait.service /usr/lib/systemd/system/zfs-volume-wait.service
+  CreateLink /etc/systemd/system/zfs.target.wants/zfs-import.target /usr/lib/systemd/system/zfs-import.target
+  CreateLink /etc/systemd/system/zfs.target.wants/zfs-mount.service /usr/lib/systemd/system/zfs-mount.service
+  CreateLink /etc/systemd/system/zfs.target.wants/zfs-volumes.target /usr/lib/systemd/system/zfs-volumes.target
 
-CreateLink /etc/systemd/system/multi-user.target.wants/zrepl.service /usr/lib/systemd/system/zrepl.service
+  CreateLink /etc/systemd/system/multi-user.target.wants/zrepl.service /usr/lib/systemd/system/zrepl.service
+fi

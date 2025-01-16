564520c10c8ef4e31614de56fab49872ce90bf07
Gordon Schulz <gordon@gordonschulz.de>
2025-01-16 12:06:10 +0100

    feat: Add gaming related packages for artemis

---
+++ 10-packages-gaming.sh
@@ -0,0 +1,16 @@
+# shellcheck shell=bash
+
+if [[ $(hostnamectl hostname) =~ artemis ]]; then
+  if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
+    sudo tee -a "/etc/pacman.conf" >/dev/null <<'EOF'
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+EOF
+    sudo pacman -Sy
+  fi
+  AddPackage lib32-vulkan-radeon # Open-source Vulkan driver for AMD GPUs - 32-bit
+  AddPackage steam               # Valve's digital software delivery system
+  AddPackage proton-cachyos      # Compatibility tool for Steam Play based on Wine and additional components, experimental branch with extra CachyOS flavour
+
+  IgnorePath "/usr/lib32"
+fi

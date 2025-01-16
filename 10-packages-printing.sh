2b0c1f01481f08260594b8dc0dec95592966680d
Gordon Schulz <gordon@gordonschulz.de>
2025-01-16 07:49:12 +0100

    feat: Add cups printers.conf filtering

--- 10-packages-printing.sh
+++ 10-packages-printing.sh
@@ -1,4 +1,8 @@
 # shellcheck shell=bash
+function CupsPrinterConfFilter() {
+  grep -v '^StateTime' | grep -v "^Attribute marker-"
+}
+
 AddPackage --foreign vuescan-bin       # A powerful proprietary scanning tool developed by Hamrick Software
 AddPackage cups                        # OpenPrinting CUPS - daemon package
 AddPackage cups-pdf                    # PDF printer for cups
@@ -6,6 +10,8 @@ AddPackage epson-inkjet-printer-escpr2 # Epson Inkjet Printer Driver 2 (ESC/P-R)
 
 CopyFile /etc/cups/ppd/EPSON_WF-4830_Series.ppd 640 '' cups
 CopyFile /etc/cups/ppd/EPSON_WF-4830_Series.ppd.O 640 '' cups
+
+AddFileContentFilter '/etc/cups/printer.conf' CupsPrinterConfFilter
 CopyFile /etc/cups/printers.conf 600
 CopyFile /etc/cups/printers.conf.O 600 '' cups
 

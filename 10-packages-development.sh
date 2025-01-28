ae3c2b758fb87b0511520715844032594db4d628
Gordon Schulz <gordon@gordonschulz.de>
2025-01-28 10:31:37 +0100

    feat: Add mise (and usage-bin optional dependency)

--- 10-packages-development.sh
+++ 10-packages-development.sh
@@ -6,7 +6,10 @@ AddPackage github-cli             # The GitHub CLI
 AddPackage go                     # Core compiler tools for the Go programming language
 AddPackage lazydocker             # A simple terminal UI for docker and docker-compose, written in Go with the gocui library.
 AddPackage lazygit                # Simple terminal UI for git commands
+AddPackage mise                   # The front-end to your dev env
 AddPackage nodejs                 # Evented I/O for V8 javascript
 AddPackage npm                    # JavaScript package manager
 AddPackage strace                 # A diagnostic, debugging and instructional userspace tracer
 AddPackage visual-studio-code-bin # Visual Studio Code (vscode): Editor for building and debugging modern web and cloud applications (official binary version)
+
+AddPackage --foreign usage-bin # A specification for CLIs

abdcc50530fc8d00349c90a05f6fe4d7b58a9def
Gordon Schulz <gordon@gordonschulz.de>
2025-02-08 13:05:20 +0100

    feat: ... and actually enable flathub and the gtk portal for it for flatpaks

---
+++ 15-flatpak.sh
@@ -0,0 +1,4 @@
+# shellcheck shell=bash
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+AddPackage xdg-desktop-portal-gtk # A backend implementation for xdg-desktop-portal using GTK

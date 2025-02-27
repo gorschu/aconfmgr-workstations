bf956fa67ca777d31fda0881c38e236ec3e120f5
Gordon Schulz <gordon@gordonschulz.de>
2025-02-27 10:31:36 +0100

    fix: Move to wezterm-git to remove bugs already fixed

--- 10-packages-terminals.sh
+++ 10-packages-terminals.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=bash
-AddPackage kitty   # A modern, hackable, featureful, OpenGL-based terminal emulator
-AddPackage ptyxis  # A terminal for a container-oriented desktop
-AddPackage wezterm # A GPU-accelerated cross-platform terminal emulator and multiplexer
+AddPackage kitty       # A modern, hackable, featureful, OpenGL-based terminal emulator
+AddPackage ptyxis      # A terminal for a container-oriented desktop
+AddPackage wezterm-git # A GPU-accelerated cross-platform terminal emulator and multiplexer

8b6abaf700f88ce74c84ec36699026313ee0890b
Gordon Schulz <gordon@gordonschulz.de>
2025-03-10 06:20:03 +0100

    feat: Add wow packages

--- 10-packages-gaming.sh
+++ 10-packages-gaming.sh
@@ -12,6 +12,9 @@ EOF
   AddPackage steam               # Valve's digital software delivery system
   AddPackage proton-cachyos      # Compatibility tool for Steam Play based on Wine and additional components, experimental branch with extra CachyOS flavour
   AddPackage protonup-qt         # Install and manage Proton-GE and Luxtorpeda for Steam and Wine-GE for Lutris
+  AddPackage wowup-cf-bin        # WowUp the World of Warcraft addon updater (with CurseForge support)
+
+  AddPackage --foreign weakauras-companion-bin # A cross-platform application built to provide the missing link between Wago.io and World of Warcraft.
 
   flatpak install flathub --user -y net.lutris.Lutris
 

479f0df320effcb18a3cd86052a7d383f97866e0
Gordon Schulz <gordon@gordonschulz.de>
2025-03-30 11:18:50 +0200

    feat: add geoclue

--- 10-packages-misc.sh
+++ 10-packages-misc.sh
@@ -1,4 +1,5 @@
 # shellcheck shell=bash
+AddPackage geoclue            # Modular geoinformation service built on the D-Bus messaging system
 AddPackage libheif            # An HEIF and AVIF file format decoder and encoder
 AddPackage tesseract-data-deu # Tesseract OCR data (deu)
 AddPackage tesseract-data-eng # Tesseract OCR data (eng)

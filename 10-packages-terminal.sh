317d258c30dfede8690141fdd0b972710cfc94ed
Gordon Schulz <gordon@gordonschulz.de>
2025-04-08 08:48:09 +0200

    feat: add gimp and exiftool

--- 10-packages-terminal.sh
+++ 10-packages-terminal.sh
@@ -1,40 +1,41 @@
 # shellcheck shell=bash
-AddPackage 7zip            # File archiver for extremely high compression
-AddPackage atool           # A script for managing file archives of various types
-AddPackage atuin           # Magical shell history
-AddPackage bat             # Cat clone with syntax highlighting and git integration
-AddPackage btop            # A monitor of system resources, bpytop ported to C++
-AddPackage cdrtools        # Highly portable CD/DVD/BluRay command line recording software
-AddPackage chafa           # Image-to-text converter supporting a wide range of symbols and palettes, transparency, animations, etc.
-AddPackage chezmoi         # Manage your dotfiles across multiple machines
-AddPackage eza             # A modern replacement for ls (community fork of exa)
-AddPackage fastfetch       # A feature-rich and performance oriented neofetch like system information tool
-AddPackage fd              # Simple, fast and user-friendly alternative to find
-AddPackage fzf             # Command-line fuzzy finder
-AddPackage gdu             # Fast disk usage analyzer
-AddPackage glow            # Command-line markdown renderer
-AddPackage htop            # Interactive process viewer
-AddPackage imagemagick     # An image viewing/manipulation program
-AddPackage just            # A handy way to save and run project-specific commands
-AddPackage less            # A terminal based program for viewing text files
-AddPackage neovim          # Fork of Vim aiming to improve user experience, plugins, and GUIs
-AddPackage renameutils     # A set of programs designed to make renaming of files faster and less cumbersome
-AddPackage starship        # The cross-shell prompt for astronauts
-AddPackage tldr            # Command line client for tldr, a collection of simplified man pages.
-AddPackage tmux            # Terminal multiplexer
-AddPackage topgrade        # Upgrade all the things
-AddPackage trash-cli       # Command line trashcan (recycle bin) interface
-AddPackage unrar           # The RAR uncompression program
-AddPackage unzip           # For extracting and viewing files in .zip archives
-AddPackage vivid           # LS_COLORS manager with multiple themes
-AddPackage xdelta3         # Diff utility for binary files
-AddPackage yazi            # Blazing fast terminal file manager written in Rust, based on async I/O
-AddPackage yt-dlp          # A youtube-dl fork with additional features and fixes
-AddPackage yubikey-manager # Python library and command line tool for configuring a YubiKey
-AddPackage zellij          # A terminal multiplexer
-AddPackage zip             # Compressor/archiver for creating and modifying zipfiles
-AddPackage zoxide          # A smarter cd command for your terminal
-AddPackage zsh             # A very advanced and programmable command interpreter (shell) for UNIX
+AddPackage 7zip                # File archiver for extremely high compression
+AddPackage atool               # A script for managing file archives of various types
+AddPackage atuin               # Magical shell history
+AddPackage bat                 # Cat clone with syntax highlighting and git integration
+AddPackage btop                # A monitor of system resources, bpytop ported to C++
+AddPackage cdrtools            # Highly portable CD/DVD/BluRay command line recording software
+AddPackage chafa               # Image-to-text converter supporting a wide range of symbols and palettes, transparency, animations, etc.
+AddPackage chezmoi             # Manage your dotfiles across multiple machines
+AddPackage eza                 # A modern replacement for ls (community fork of exa)
+AddPackage fastfetch           # A feature-rich and performance oriented neofetch like system information tool
+AddPackage fd                  # Simple, fast and user-friendly alternative to find
+AddPackage fzf                 # Command-line fuzzy finder
+AddPackage gdu                 # Fast disk usage analyzer
+AddPackage glow                # Command-line markdown renderer
+AddPackage htop                # Interactive process viewer
+AddPackage imagemagick         # An image viewing/manipulation program
+AddPackage just                # A handy way to save and run project-specific commands
+AddPackage less                # A terminal based program for viewing text files
+AddPackage neovim              # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage perl-image-exiftool # Reader and rewriter of EXIF information that supports raw files
+AddPackage renameutils         # A set of programs designed to make renaming of files faster and less cumbersome
+AddPackage starship            # The cross-shell prompt for astronauts
+AddPackage tldr                # Command line client for tldr, a collection of simplified man pages.
+AddPackage tmux                # Terminal multiplexer
+AddPackage topgrade            # Upgrade all the things
+AddPackage trash-cli           # Command line trashcan (recycle bin) interface
+AddPackage unrar               # The RAR uncompression program
+AddPackage unzip               # For extracting and viewing files in .zip archives
+AddPackage vivid               # LS_COLORS manager with multiple themes
+AddPackage xdelta3             # Diff utility for binary files
+AddPackage yazi                # Blazing fast terminal file manager written in Rust, based on async I/O
+AddPackage yt-dlp              # A youtube-dl fork with additional features and fixes
+AddPackage yubikey-manager     # Python library and command line tool for configuring a YubiKey
+AddPackage zellij              # A terminal multiplexer
+AddPackage zip                 # Compressor/archiver for creating and modifying zipfiles
+AddPackage zoxide              # A smarter cd command for your terminal
+AddPackage zsh                 # A very advanced and programmable command interpreter (shell) for UNIX
 
 AddPackage --foreign clipboard-bin # Cut, copy, and paste anything in your terminal (binary version).
 AddPackage --foreign filen-cli-bin # The Filen CLI provides a set of useful tools for interacting with the cloud.

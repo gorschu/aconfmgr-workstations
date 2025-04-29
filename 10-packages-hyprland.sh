086820adf2e3df1f39179708f52b45b66d6688a3
Gordon Schulz <gordon@gordonschulz.de>
2025-04-29 12:11:11 +0200

    feat: Add hyprland packages

---
+++ 10-packages-hyprland.sh
@@ -0,0 +1,11 @@
+# shellcheck shell=bash
+AddPackage brillo   # Control the brightness of backlight and keyboard LED devices
+AddPackage ddcutil  # Query and change Linux monitor settings using DDC/CI and USB.
+AddPackage hypridle # hyprland’s idle daemon
+AddPackage hyprland # a highly customizable dynamic tiling Wayland compositor
+AddPackage hyprlock # hyprland’s GPU-accelerated screen locking utility
+AddPackage uwsm     # A standalone Wayland session manager
+AddPackage waybar   # Highly customizable Wayland bar for Sway and Wlroots based compositors
+
+AddPackage --foreign hyprland-plugin-hyprexpo # A Hyprland plugin that adds an expo-like workspace overview
+AddPackage --foreign walker-bin               # wayland application runner

afd1df807f42899c95af0d8f2ce6029377873eaa
Gordon Schulz <gordon@gordonschulz.de>
2025-05-09 11:49:49 +0200

    fix: Make flatpak handling more robust

--- 15-flatpak.sh
+++ 15-flatpak.sh
@@ -1,4 +1,8 @@
 # shellcheck shell=bash
-flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+AconfNeedProgram flatpak flatpak n
 
 AddPackage xdg-desktop-portal-gtk # A backend implementation for xdg-desktop-portal using GTK
+
+if ! flatpak remotes | grep -q "flathub.*user"; then
+  flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+fi

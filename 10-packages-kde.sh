e42f6989f50847a46180f86e4508a196e9bdf18a
Gordon Schulz <gordon@gordonschulz.de>
2025-05-09 11:49:09 +0200

    chore: Add more kde packages

--- 10-packages-kde.sh
+++ 10-packages-kde.sh
@@ -1,5 +1,7 @@
 # shellcheck shell=bash
+AddPackage ark                      # Archiving Tool
 AddPackage dolphin                  # KDE File Manager
+AddPackage kate                     # Advanced text editor
 AddPackage kdeconnect               # Adds communication between KDE and your smartphone
 AddPackage kdegraphics-thumbnailers # Thumbnailers for various graphics file formats
 AddPackage kdialog                  # A utility for displaying dialog boxes from shell scripts
@@ -9,6 +11,7 @@ AddPackage kwalletmanager           # Wallet management tool
 AddPackage gwenview                 # A fast and easy to use image viewer
 AddPackage okular                   # Document Viewer
 AddPackage plasma-meta              # Meta package to install KDE Plasma
+AddPackage plasma-workspace         # KDE Plasma workspace
 AddPackage qt6-multimedia-ffmpeg
 
 CreateFile /etc/sddm.conf >/dev/null

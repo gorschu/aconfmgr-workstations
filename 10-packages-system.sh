e3e4603f85560ff74cf20ad95e1703fac0e07471
Gordon Schulz <gordon@gordonschulz.de>
2025-05-09 10:30:26 +0200

    chore: Switch from cachyos kernels to arch kernels

--- 10-packages-system.sh
+++ 10-packages-system.sh
@@ -7,9 +7,8 @@ AddPackage dosfstools              # DOS filesystem utilities
 AddPackage efibootmgr              # Linux user-space application to modify the EFI Boot Manager
 AddPackage fwupd                   # Simple daemon to allow session software to update firmware
 AddPackage gptfdisk                # A text-mode partitioning tool that works on GUID Partition Table (GPT) disks
-AddPackage linux                   # The Linux kernel and modules
-AddPackage linux-cachyos           # The Linux BORE + LTO + AutoFDO Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel and modules
-AddPackage linux-cachyos-lts       # The Linux BORE + Cachy Sauce Kernel by CachyOS with other patches and improvements - Long Term Service kernel and modules
+AddPackage linux-lts               # The LTS Linux kernel and modules
+AddPackage linux-zen               # The Linux ZEN kernel and modules
 AddPackage linux-firmware          # Firmware files for Linux
 AddPackage man-db                  # A utility for reading man pages
 AddPackage sbsigntools             # Tools to add signatures to EFI binaries and Drivers

314d4244c1b7e46ded4f22894f1800d5f8d206d1
Gordon Schulz <gordon@gordonschulz.de>
2025-05-10 18:10:36 +0200

    chore: add pacman-contrib

--- 05-packages-arch.sh
+++ 05-packages-arch.sh
@@ -1,5 +1,6 @@
 # shellcheck shell=bash
-AddPackage aconfmgr-git          # A configuration manager for Arch Linux
-AddPackage paru-bin              # Feature packed AUR helper
-AddPackage pkgfile               # alpm .files metadata explorer
-AddPackage reflector             # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
+AddPackage aconfmgr-git   # A configuration manager for Arch Linux
+AddPackage pacman-contrib # Contributed scripts and tools for pacman systems
+AddPackage paru-bin       # Feature packed AUR helper
+AddPackage pkgfile        # alpm .files metadata explorer
+AddPackage reflector      # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.

a24f7c9f9118a2d9d924a3b698fd54cf07b73564
Gordon Schulz <gordon@gordonschulz.de>
2025-07-01 15:16:56 +0200

    chore: Remove archinstall kernel cmdline file. hard.

--- 20-kernel-root.sh
+++ 20-kernel-root.sh
@@ -4,3 +4,6 @@
 
 rootUUID=$(get_root_UUID "$(hostnamectl hostname)")
 echo "rd.luks.uuid=${rootUUID} root=/dev/mapper/luks-${rootUUID} rw rootflags=subvol=@ rootfstype=btrfs" >"$(CreateFile /etc/cmdline.d/root.conf)"
+
+# remove old /etc/kernel/cmdline from archinstall
+rm -f /etc/kernel/cmdline
